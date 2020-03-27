1f6ba53bab2139941544e0a5f7fd74175813b10b
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-03-27 01:06:09 -0600

    Finished demo

---
+++ index.js
@@ -0,0 +1,36 @@
+let intentos = 0;
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+// user input
+const userNum = document.getElementById("userNum");
+const guessBtn = document.getElementById("guessButton");
+
+// containers
+const cluesContainer = document.getElementById("clues");
+const congratsContainer = document.getElementById("congrats");
+
+// game info
+const tries = document.getElementById("tries");
+const clue = document.getElementById("clue");
+
+guessBtn.addEventListener(
+  "click",
+  () => {
+    const num = userNum.value;
+
+    if (num == randomNumber) {
+      congratsContainer.style.display = "flex";
+      cluesContainer.style.display = "none";
+      guessBtn.style.display = "none";
+      userNum.disabled = true;
+      return;
+    }
+
+    intentos = intentos + 1;
+
+    cluesContainer.style.display = "flex";
+    tries.innerHTML = intentos;
+    clue.innerHTML = num > randomNumber ? "👎🏼" : "👍🏼";
+  },
+  false
+);
